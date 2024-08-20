0bca7866942fbe39e2be438a37087110c70af95e
amir <amirh9476@gmail.com>
2024-08-20 18:33:43 +0330

    add serializers and view subscription too payments

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from products import views
 app_name = 'products'
 urlpatterns  = [
-    path('product/', views.ProductListView.as_view(), name='product-list'),
+    path('', views.ProductListView.as_view(), name='product-list'),
     path('product/<int:pk>/', views.ProductDetailView.as_view(), name='product-detail'),
     path('category/', views.CategoryListView.as_view(), name='category-list'),
     path('category/<int:pk>/', views.CategoryDetailView.as_view(), name='category-detail'),
